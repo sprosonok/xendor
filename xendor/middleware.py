4bc8a256103aea75646cd6f760fe2f2c834e2526
Потапов Павел <tahy@iMac-apple.local>
2013-11-17 15:24:13 +0600

    inwork

--- xendor/middleware.py
+++ xendor/middleware.py
@@ -7,17 +7,18 @@ from xendor.settings import XendorSettings
 
 
 class CmsCoreMiddleware(object):
-
+    """closing site for  XENDOR_TEST_MODE=True"""
     def process_request(self, request):
-        if request.META['PATH_INFO'].find('.') is -1:
-            if settings.XDP_TEST_MODE and request.META['PATH_INFO'].find('/admin/') is -1:
-                if not request.user.is_staff and not request.META['PATH_INFO'] == reverse('xendor-closed'):
-                    return HttpResponseRedirect(reverse('xendor-closed'))
-            elif request.META['PATH_INFO'] == reverse('xendor-closed'):
-                return HttpResponseRedirect(reverse('main-page'))
+
+        if settings.XENDOR_TEST_MODE and request.META['PATH_INFO'].find('/admin/') is -1:
+            if not request.user.is_staff and not request.META['PATH_INFO'] == reverse('xendor-closed'):
+                return HttpResponseRedirect(reverse('xendor-closed'))
+        elif request.META['PATH_INFO'] == reverse('xendor-closed'):
+            return HttpResponseRedirect(reverse('main-page'))
 
 
 class XendorSettingMiddleware(object):
+    """middleware for setting correct working"""
 
     def process_request(self, request):
         XendorSettings().clear_meta()
--- xendor/ptemplate/project_name/settings.py
+++ xendor/ptemplate/project_name/settings.py
@@ -1,60 +1,76 @@
 # -*- coding: utf-8 -*-
-# Django settings for {{ project_name }} project.
-import os, sys
+"""
+Django settings for {{ project_name }} project.
 
-NEED_REGENERATE_MODELS = ['Page',]
-XDP_TEST_MODE = False #site closing
+For more information on this file, see
+https://docs.djangoproject.com/en/{{ docs_version }}/topics/settings/
 
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '{{ secret_key }}'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+NEED_REGENERATE_MODELS = ['Page', ]
+XENDOR_TEST_MODE = False
 
 ADMINS = (
     ('admin', 'sprosonok@gmail.com'),
     ('tahy', 'tahy.gm@gmail.com'),
 )
 
-MANAGERS = ADMINS
+# Application definition
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '{{ project_name }}',          # Or path to database file if using sqlite3.
-        'USER': 'root',                        # Not used with sqlite3.
-        'PASSWORD': 'RewyM6',                  # Not used with sqlite3.
-        'HOST': '85.12.240.187',                # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3306',                        # Set to empty string for default. Not used with sqlite3.
-        'TEST_CHARSET': 'utf8',
-        'TEST_COLLATION': 'utf8_general_ci',
-    }
-}
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 
-SITE_URL = '127.0.0.1'
-TIME_ZONE = 'Asia/Yekaterinburg'
-LANGUAGE_CODE = 'ru-RU'
-SITE_ID = 1
+    'south',
 
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
+    'mptt',
+    'mpttadmin',
 
+    'tinymce',
 
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'public', 'media/').replace('\\','/')
-MEDIA_URL = '/media/'
+    'filebrowser',
 
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'public', 'static').replace('\\','/')
-STATIC_URL = '/static/'
+    'xendor',
 
-STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'),
+    ###===user-generated-apps===###
 )
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '{{ secret_key }}'
+    #xendor specific middleware
+    'xendor.middleware.XendorSettingMiddleware',
+    'xendor.middleware.CmsCoreMiddleware',
+)
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
@@ -67,83 +83,79 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.csrf",
 )
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-
-    #site settings
-    'xendor.middleware.XendorSettingMiddleware',
-    'xendor.middleware.CmsCoreMiddleware',
-
-    # site middleware
-    '{{ project_name }}.middleware.SiteMiddleware',
-)
-
 ROOT_URLCONF = '{{ project_name }}.urls'
 
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+
+# Database
+# https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '{{ project_name }}',          # Or path to database file if using sqlite3.
+        'USER': 'root',                        # Not used with sqlite3.
+        'PASSWORD': 'changeme',                  # Not used with sqlite3.
+        'HOST': '5.172.14.177',                # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                        # Set to empty string for default. Not used with sqlite3.
+        'TEST_CHARSET': 'utf8',
+        'TEST_COLLATION': 'utf8_general_ci',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/{{ docs_version }}/topics/i18n/
+
+LANGUAGE_CODE = 'ru-RU'
+
+TIME_ZONE = 'Asia/Yekaterinburg'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images) and media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media')
+MEDIA_URL = '/media/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'public/static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
 )
 
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'xdp_admin',
-    'django.contrib.sitemaps',
-
-    ###===collection modules===###
-    'supercaptcha',
-    'south',
-
-    #tree processing
-    'mptt',
-    'mpttadmin',
-
-    #js-editor
-    'tinymce',
-
-    #CMS kernel
-    'xendor',
-    'filebrowser',
-
-    ###===clients modules===###
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
 )
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+NO_IMG_PATH = 'no_img.png'
+
 TINYMCE_DEFAULT_CONFIG = {
-    'plugins' : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+    'plugins': "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
     'theme': "advanced",
-    'theme_advanced_buttons1' : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,|,styleselect,formatselect",
-    'theme_advanced_buttons2' : "cut,copy,paste,pasteword,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,code,|,forecolor,backcolor",
-    'theme_advanced_buttons3' : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,iespell,media,advhr,|,fullscreen",
-    'theme_advanced_toolbar_location' : "top",
-    'theme_advanced_toolbar_align' : "left",
-    'theme_advanced_statusbar_location' : "bottom",
-    'theme_advanced_resizing' : "true",
-    'relative_urls' : False,
+    'theme_advanced_buttons1': "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,|,styleselect,formatselect",
+    'theme_advanced_buttons2': "cut,copy,paste,pasteword,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,code,|,forecolor,backcolor",
+    'theme_advanced_buttons3': "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,iespell,media,advhr,|,fullscreen",
+    'theme_advanced_toolbar_location': "top",
+    'theme_advanced_toolbar_align': "left",
+    'theme_advanced_statusbar_location': "bottom",
+    'theme_advanced_resizing': "true",
+    'relative_urls': False,
 }
 
-MULTILANGUAL = SHOW_CHUNK_CATEGORIES = False
 
-sys.path.append(os.path.join('c:\\distr','lib'))
-sys.path.append(os.path.join('/home/tahy/workspace/development/python/libs','lib'))
-sys.path.append(os.path.join('/Users/tahy/workspace/','lib'))
+import sys
+sys.path.append(os.path.join('/Users/tahy/workspace', 'lib'))
+sys.path.append(os.path.join('/Users/tahy/workspace/lib', 'xendor'))
+sys.path.append(os.path.join('/Users/tahy/workspace/lib', 'django-filebrowser-no-grappelli'))
+
+
